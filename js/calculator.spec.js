099cf3b8c32e7bbbf2c4043de85608ea82223562
rex <retwedt@gmail.com>
2018-11-30 22:23:22 -0600

    Expand the tests to include vert/diag FOV dimensions.

--- js/calculator.spec.js
+++ js/calculator.spec.js
@@ -42,14 +42,40 @@ Test.isEqual(radiansToDegrees((3 * Math.PI) / 2), 270);
 Test.isEqual(radiansToDegrees(2 * Math.PI), 360);
 console.log("\n");
 
+// Save the fovDimen value so we can reset it when the tests are finished.
+const fovDimenBackup = fovDimen;
+
 console.log("fovToZoom");
-Test.isEqual(fovToZoom(1920, 1080, 45), 2317.6450198781713);
-Test.isEqual(fovToZoom(854, 480, 45), 1030.8691911333117);
+fovDimen = "horz";
+console.log("fovDimen == horz");
+Test.isEqual(fovToZoom(1920, 1080, 45), 2317.6450198781712468496211752413);
+Test.isEqual(fovToZoom(854, 480, 45), 1030.8691911333115858383210852375);
+fovDimen = "vert";
+console.log("fovDimen == vert");
+Test.isEqual(fovToZoom(1920, 1080, 45), 1303.6753236814713263529119110732);
+Test.isEqual(fovToZoom(854, 480, 45), 579.41125496954281171240529381033);
+fovDimen = "diag";
+console.log("fovDimen == diag");
+Test.isEqual(fovToZoom(1920, 1080, 45), 2659.144183330809504153724670819);
+Test.isEqual(fovToZoom(854, 480, 45), 1182.5433148993861036361943290341);
 console.log("\n");
 
 console.log("zoomToFov");
-Test.isEqual(zoomToFov(1920, 1080, 2317.645), 45.00000034748587);
-Test.isEqual(zoomToFov(854, 480, 1030.869), 45.00000751173966);
+fovDimen = "horz";
+console.log("fovDimen == horz");
+Test.isEqual(zoomToFov(1920, 1080, 2317.6450198781712468496211752413), 45);
+Test.isEqual(zoomToFov(854, 480, 1030.8691911333115858383210852375), 45);
+fovDimen = "vert";
+console.log("fovDimen == vert");
+Test.isEqual(zoomToFov(1920, 1080, 1303.6753236814713263529119110732), 45);
+Test.isEqual(zoomToFov(854, 480, 579.41125496954281171240529381033), 45);
+fovDimen = "diag";
+console.log("fovDimen == diag");
+Test.isEqual(zoomToFov(1920, 1080, 2659.144183330809504153724670819), 45);
+Test.isEqual(zoomToFov(854, 480, 1182.5433148993861036361943290341), 45);
 console.log("\n");
+
+// Reset fovDimen!
+fovDimen = fovDimenBackup;
 
 console.log(" ----- End Tests ----- ");
